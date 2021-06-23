f49dbfc47d22ace4b74aa9b27a7e6eca4f2deea0
Jake <cocahack@naver.com>
2021-06-23 14:24:18 +0900

    feat: 값이 있을때만 twitter 링크 생성하도록 변경

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -11,14 +11,17 @@ const Author = () => {
     <div className={styles['author']}>
       <p className={styles['author__bio']}>
         {author.bio}
-        <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <strong>{author.name}</strong> on Twitter
-        </a>
+        { author.contacts.twitter
+          ? (
+            <a className={styles['author__bio-twitter']}
+               href={getContactHref('twitter', author.contacts.twitter)}
+               rel="noopener noreferrer"
+               target="_blank"
+            >
+              <strong>{author.name}</strong> on Twitter
+            </a>
+          ) : ''
+        }
       </p>
     </div>
   );
