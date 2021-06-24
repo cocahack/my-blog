9d81f20ec24fea971343d5c42aec8f72f93fb635
Jake <cocahack@naver.com>
2021-06-24 22:51:40 +0900

    fix: 링크 카드가 하나만 생성되도록 수정

--- plugins/gatsby-remark-my-unfurl/index.js
+++ plugins/gatsby-remark-my-unfurl/index.js
@@ -3,25 +3,20 @@ const { selectAll } = require('unist-util-select');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = async ({ markdownAST }, _pluginOptions) => {
-  const links = selectAll('paragraph link:only-child', markdownAST);
+  const allParagraphs = selectAll('paragraph:has(:only-child)', markdownAST)
+    .filter((node) => node.children[0].type === 'link');
 
-  console.log('before!!!!!!!!!');
-  console.dir(markdownAST, { depth: 10 });
   await Promise.all(
-    links.map((link) => unfurlFromLink(link))
+    allParagraphs.map((paragraph) => unfurlFromLink(paragraph))
   );
-  console.log('after!!!!!!!!!');
-  console.dir(markdownAST, { depth: 10 });
 };
 
-const unfurlFromLink = async (linkNode) => {
-  const { url } = linkNode;
+const unfurlFromLink = async (paragraphNode) => {
+  const { url } = paragraphNode.children[0];
   const metadata = extractLinkMetadata(await unfurl(url));
   metadata.url = url;
 
-  console.dir(metadata, { depth: 4 });
-
-  return convertLinkToCardElement(linkNode, metadata);
+  return convertLinkToCardElement(paragraphNode, metadata);
 };
 
 const extractLinkMetadata = (metadata) => ({
@@ -46,37 +41,25 @@ const extractLinkMetadata = (metadata) => ({
       || undefined,
 });
 
-// TODO: HTML 타입을 쓰는게 아니라, AST 를 직접 만들어줘야 할 것 같다. 아니면 Link 를 가진 Paragraph 자체를 제거하고, 이걸로 대체하는 것도 방법이다.
-/*
-https://www.gatsbyjs.com/tutorial/remark-plugin-tutorial/
-https://github.com/syntax-tree/mdast
-https://astexplorer.net/
-https://github.com/hgezim/gatsby-remark-link-unfurl
-
-참고하기
- */
-const convertLinkToCardElement = (linkNode, metadata) => {
-  linkNode.type = 'html';
-  linkNode.value = `
+const convertLinkToCardElement = (paragraphNode, metadata) => {
+  paragraphNode.type = 'html';
+  paragraphNode.value = `
     <a class='gatsby-remark-link-unfurl__container' href='${metadata.url}' target='_blank' title='${metadata.title}'>
-      <div class='gatsby-remark-link-unfurl__media' style='background-image: url(${metadata.image?.url}'></div>
+      <img class='gatsby-remark-link-unfurl__media' src='${metadata.image?.url}'  alt="link image"/>
       <div class='gatsby-remark-link-unfurl__content'>
-      <header class='gatsby-remark-link-unfurl__title'><p  title='${metadata.title}'>${metadata.title}</p></header>
+        <header class='gatsby-remark-link-unfurl__title'><p  title='${metadata.title}'>${metadata.title}</p></header>
         <div class='gatsby-remark-link-unfurl__description'><p title='${metadata.description}'>${metadata.description}</p></div>
-      <footer>
-        <p title='${metadata.site}'>
-            ${metadata.site}
-        </p>
-        <span title='${metadata.site}' style='background-image: url(${metadata.logo}); background-repeat: no-repeat; background-position: center center;'>
-        </span>
-      </footer>
-    </div>
+        <footer>
+          <p title='${metadata.site}'>
+              ${metadata.site}
+          </p>
+          <span title='${metadata.site}' style='background-image: url(${metadata.logo}); background-repeat: no-repeat; background-position: center center;'>
+          </span>
+        </footer>
+      </div>
     </a>
   `;
-  delete linkNode.children;
-  delete linkNode.url;
-  delete linkNode.title;
-  delete linkNode.data;
+  delete paragraphNode.children;
 
-  return linkNode;
+  return paragraphNode;
 };
