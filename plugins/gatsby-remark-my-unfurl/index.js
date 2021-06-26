8aa7180222f3be43efdfde229196884bd98cd560
Jake <cocahack@naver.com>
2021-06-26 20:13:42 +0900

    feat: mustache 템플릿 적용

--- plugins/gatsby-remark-my-unfurl/index.js
+++ plugins/gatsby-remark-my-unfurl/index.js
@@ -1,5 +1,6 @@
 const { unfurl } = require('unfurl.js');
 const { selectAll } = require('unist-util-select');
+const Mustache = require('mustache');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = async ({ markdownAST }, _pluginOptions) => {
@@ -28,38 +29,51 @@ const extractLinkMetadata = (metadata) => ({
       metadata.twitter_card?.description
       ?? metadata.open_graph?.description
       ?? metadata.description,
-  video: metadata.open_graph?.videos?.[0] || undefined,
+  video: metadata.open_graph?.videos?.[0] || '',
   image:
       metadata.twitter_card?.images?.[0]
       || metadata.open_graph?.images?.[0]
-      || undefined,
-  logo: metadata.favicon,
+      || '',
+  logo: {
+    url: metadata.favicon,
+    site:
+      metadata.oEmbed?.provider_name
+      || metadata.open_graph?.site_name
+      || metadata.twitter_card?.site
+      || '',
+  },
   site:
       metadata.oEmbed?.provider_name
       || metadata.open_graph?.site_name
       || metadata.twitter_card?.site
-      || undefined,
+      || '',
 });
 
 const convertLinkToCardElement = (paragraphNode, metadata) => {
   paragraphNode.type = 'html';
-  paragraphNode.value = `
-    <a class='gatsby-remark-link-unfurl__container' href='${metadata.url}' target='_blank' title='${metadata.title}'>
-      <img class='gatsby-remark-link-unfurl__media' src='${metadata.image?.url}'  alt="link image"/>
-      <div class='gatsby-remark-link-unfurl__content'>
-        <header class='gatsby-remark-link-unfurl__title'><p  title='${metadata.title}'>${metadata.title}</p></header>
-        <div class='gatsby-remark-link-unfurl__description'><p title='${metadata.description}'>${metadata.description}</p></div>
-        <footer>
-          <p title='${metadata.site}'>
-              ${metadata.site}
-          </p>
-          <span title='${metadata.site}' style='background-image: url(${metadata.logo}); background-repeat: no-repeat; background-position: center center;'>
-          </span>
-        </footer>
-      </div>
-    </a>
-  `;
+  paragraphNode.value = Mustache.render(template, metadata);
   delete paragraphNode.children;
 
   return paragraphNode;
 };
+
+const template = `
+  <a class='gatsby-remark-link-unfurl__container' href='{{url}}' target='_blank' title='{{title}}'>
+    {{#image}}
+      <img class='gatsby-remark-link-unfurl__media' src='{{url}}'  alt="link image"/>
+    {{/image}}  
+    <div class='gatsby-remark-link-unfurl__content'>
+      <header class='gatsby-remark-link-unfurl__title'><p  title='{{title}}'>{{title}}</p></header>
+      <div class='gatsby-remark-link-unfurl__description'><p title='{{description}}'>{{description}}</p></div>
+      <footer>
+        {{#logo}}
+          <span title='{{site}}' style='background-image: url({{url}}); background-repeat: no-repeat; background-position: center center;'>
+          </span>
+        {{/logo}}
+        <div title='{{site}}'>
+            {{site}}
+        </div>
+      </footer>
+    </div>
+  </a>
+`;
